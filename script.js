f074d1ffe398184b737fdc6ea8445ead216a469b
kdbrdva-ship-it <kdbrdva@gmail.com>
2026-08-02 11:21:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const envelope = document.querySelector(".envelope");
+
+envelope.addEventListener("click", function() {
+    alert("Моей любимой сестре ❤️ Скоро здесь появится настоящее письмо!");
+});
