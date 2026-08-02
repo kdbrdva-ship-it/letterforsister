952475c5e76b468160918dde69d876aeed762b2d
kdbrdva-ship-it <kdbrdva@gmail.com>
2026-08-02 18:14:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const envelope = document.querySelector(".envelope");
 
-const seal = document.querySelector(".seal");
-
 const flap = document.querySelector(".flap");
+
+const seal = document.querySelector(".seal");
 
 const letter = document.querySelector(".letter");
 
 
+
 envelope.addEventListener("click", () => {
 
-    envelope.classList.toggle("open");
 
-    seal.classList.toggle("hide");
+flap.classList.toggle("open-flap");
 
-    flap.classList.toggle("open-flap");
 
-    letter.classList.toggle("show-letter");
+letter.classList.toggle("show-letter");
+
+
+seal.classList.toggle("hide");
+
 
 });
